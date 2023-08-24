e0c699b4e8e233fac0bcb376fc20e516feba449d
gareth-39 <Gareth.A.Henvey@gmail.com>
2023-08-24 14:04:59 +0000

    fourth Error deploying

--- retro_football_kits/settings.py
+++ retro_football_kits/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-hn75!$dh9=p$m)#j3+x+kmf)@)=-l23t(4s2^ut!h_5h^04p2g
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['retro-football-kits-17e2f93d4b27.herokuapp.com/', 'localhost']
+ALLOWED_HOSTS = ['retro-football-kits-17e2f93d4b27.herokuapp.com']
 
 
 # Application definition
