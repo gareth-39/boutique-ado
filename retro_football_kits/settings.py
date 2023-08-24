19c83f6aae3655326f735be499b2d602ce691366
gareth-39 <Gareth.A.Henvey@gmail.com>
2023-08-24 16:55:19 +0000

    Changed my secret key

--- retro_football_kits/settings.py
+++ retro_football_kits/settings.py
@@ -24,10 +24,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-hn75!$dh9=p$m)#j3+x+kmf)@)=-l23t(4s2^ut!h_5h^04p2g'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = 'DEVELOPMENT' in os.environ
+
 
 ALLOWED_HOSTS = ['retro-football-kits-17e2f93d4b27.herokuapp.com', 'localhost']
 
