185ab2b33f94da789eb00d8b8613f384bd4410f5
gareth-39 <Gareth.A.Henvey@gmail.com>
2023-08-24 13:17:19 +0000

    first deployment

--- retro_football_kits/settings.py
+++ retro_football_kits/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-hn75!$dh9=p$m)#j3+x+kmf)@)=-l23t(4s2^ut!h_5h^04p2g
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['8000-gareth39-retrofootballk-xzjfuvdpyoz.ws-eu104.gitpod.io']
+ALLOWED_HOSTS = ['retro-football-kits.heroku.com', 'localhost']
 
 
 # Application definition
