6154b92e0c057f29e603c75fecb6779ec82627f5
gareth-39 <Gareth.A.Henvey@gmail.com>
2023-08-27 14:57:14 +0000

    Adding product image

--- retro_football_kits/urls.py
+++ retro_football_kits/urls.py
@@ -17,7 +17,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-
+from .views import handler404
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -29,4 +29,4 @@ urlpatterns = [
     path('profile/', include('profiles.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
-
+handler404 = 'retro-football-kits.views.handler404'
