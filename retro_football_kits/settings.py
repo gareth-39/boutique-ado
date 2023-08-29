33008393351963051b4cb493a263ebb39acb1677
gareth-39 <Gareth.A.Henvey@gmail.com>
2023-08-29 03:46:49 +0000

    Adding footer html

--- retro_football_kits/settings.py
+++ retro_football_kits/settings.py
@@ -35,7 +35,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['retro-football-kits-17e2f93d4b27.herokuapp.com', 'localhost']
 
-
+'retro-football-kits-17e2f93d4b27.herokuapp.com'
 # Application definition
 
 INSTALLED_APPS = [
@@ -190,7 +190,7 @@ if 'USE_AWS' in os.environ:
 
     # Bucket Config
     AWS_STORAGE_BUCKET_NAME = 'retro-football-kits'
-    AWS_S3_REGION_NAME = 'us-east-1'
+    AWS_S3_REGION_NAME = 'eu-west-1'
     AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
     AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
     AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
