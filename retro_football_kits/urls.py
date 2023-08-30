d5231b1e5c90975dd730f7b1df0884b37b34f55a
gareth-39 <Gareth.A.Henvey@gmail.com>
2023-08-30 16:27:10 +0000

    Adding privacy and contact to the footer

--- retro_football_kits/urls.py
+++ retro_football_kits/urls.py
@@ -28,6 +28,8 @@ urlpatterns = [
     path('bag/', include('bag.urls')),
     path('checkout/', include('checkout.urls')),
     path('profile/', include('profiles.urls')),
+    path('privacy/', include('privacy.urls')),
+    path('contact/', include('contact.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 handler404 = 'retro_football_kits.views.handler404'
