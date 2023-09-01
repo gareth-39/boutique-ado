a09d10005ddd08f5e013b40088f5204c6b99b33f
gareth-39 <Gareth.A.Henvey@gmail.com>
2023-09-01 01:27:55 +0000

    Site ready for testing

--- retro_football_kits/settings.py
+++ retro_football_kits/settings.py
@@ -30,12 +30,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 
 ALLOWED_HOSTS = ['retro-football-kits-17e2f93d4b27.herokuapp.com', 'localhost']
 
-'retro-football-kits-17e2f93d4b27.herokuapp.com'
+
 # Application definition
 
 INSTALLED_APPS = [
