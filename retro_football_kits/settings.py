2182880ab3a8fa2200337121923cc1e2db37b36a
gareth-39 <Gareth.A.Henvey@gmail.com>
2023-09-06 08:08:36 +0000

    Continuing  READ.me file

--- retro_football_kits/settings.py
+++ retro_football_kits/settings.py
@@ -30,10 +30,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = 'DEVELOPMENT' in os.environ
+DEBUG = True
 
 
-ALLOWED_HOSTS = ['retro-football-kits-17e2f93d4b27.herokuapp.com', 'localhost']
+ALLOWED_HOSTS = [ '8000-gareth39-retrofootballk-xzjfuvdpyoz.ws-eu104.gitpod.io', 'localhost']
 
 
 # Application definition
