f32f5065811cdfd03eac59e6c72fe6dd70c71804
gareth-39 <Gareth.A.Henvey@gmail.com>
2023-11-13 05:46:04 +0000

    testing deploy for confirmation email

--- retro_football_kits/settings.py
+++ retro_football_kits/settings.py
@@ -222,7 +222,7 @@ STRIPE_WH_SECRET = os.getenv('STRIPE_WH_SECRET', '')
 
 if 'DEVELOPMENT' in os.environ:
     EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
-    DEFAULT_FROM_EMAIL = 'gareth.a.henvey.example.com'
+    DEFAULT_FROM_EMAIL = 'retrofootballkits.example.com'
 else:
     EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
     EMAIL_USE_TLS = True
